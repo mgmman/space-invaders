9f697c490870cde74f7d9734c91ca9b1a46a2983
Gurikov Maksim <maximgurikoff@gmail.com>
2023-05-01 17:04:13 +0500

    added health system

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import pygame
 import os
 import time
+import random
 
 class Game:
     screen = None
-    aliens = []
+    invaders = []
+    invader_rockets = []
     rockets = []
     lost = False
 
@@ -21,13 +23,13 @@ class Game:
 
         player = Player(self, width / 2, height-100)
         self.all_sprites.add(player)
-        generator = Generator(self)
+        self.generate_invaders()
         rocket = None
 
-        while not done:
-            if len(self.aliens) == 0:
-                self.displayText("VICTORY ACHIEVED")
+        self.run_game(done, height, player, width)
 
+    def run_game(self, done, height, player, width):
+        while not done:
             pressed = pygame.key.get_pressed()
             if pressed[pygame.K_LEFT]:
                 player.rect.x -= 2 if player.rect.x > 20 else 0
@@ -44,18 +46,44 @@ class Game:
             self.clock.tick(60)
             self.screen.fill((0, 0, 0))
 
-            if len(self.aliens[-1]) == 0:
-                self.aliens.pop()
-            if self.aliens[-1][0].rect.y > height-100:
-                self.lost = True
-                self.displayText("YOU DIED")
+            if len(self.invaders) == 0:
+                self.displayText("VICTORY ACHIEVED")
+            else:
+                if len(self.invaders[-1]) == 0:
+                    self.invaders.pop()
 
-            for rocket in self.rockets:
-                rocket.draw()
+                if self.invaders[-1][0].rect.y > height - 100 or player.health <= 0:
+                    self.lost = True
+                    self.displayText("DEFEAT")
+
+                if random.random() > 0.98:
+                    shooting_invader = random.choice(self.invaders[-1])
+                    invader_rocket = InvaderRocket(self, shooting_invader.rect.center[0], shooting_invader.rect.center[1])
+                    self.invader_rockets.append(invader_rocket)
 
             if not self.lost:
                 self.all_sprites.update()
                 self.all_sprites.draw(self.screen)
+
+                for rocket in self.rockets:
+                    rocket.draw()
+
+                for rocket in self.invader_rockets:
+                    rocket.draw();
+
+    def generate_invaders(self):
+        vertical_margin = 30
+        horizontal_margin = 150
+        width = 50
+        row = -1
+        for y in range(horizontal_margin, int(self.height / 2), width):
+            row += 1
+            invaders_row = []
+            for x in range(vertical_margin, self.width - vertical_margin, width):
+                invader = Invader(self, x, y, row)
+                invaders_row.append(invader)
+                self.all_sprites.add(invader)
+            self.invaders.append(invaders_row)
 
     def displayText(self, text):
         pygame.font.init()
@@ -64,7 +92,7 @@ class Game:
         self.screen.blit(textsurface, (110, 160))
 
 
-class Alien(pygame.sprite.Sprite):
+class Invader(pygame.sprite.Sprite):
     def __init__(self, game, x, y, row):
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.image.load(os.path.join(game.image_folder, 'invader.png')).convert()
@@ -89,7 +117,7 @@ class Alien(pygame.sprite.Sprite):
             if (self.rect.x + self.size > rocket.x > self.rect.x - self.size and
                     self.rect.y + self.size > rocket.y > self.rect.y - self.size):
                 game.rockets.remove(rocket)
-                game.aliens[self.row].remove(self)
+                game.invaders[self.row].remove(self)
                 game.all_sprites.remove(self)
 
 
@@ -99,34 +127,19 @@ class Player(pygame.sprite.Sprite):
         self.image = pygame.image.load(os.path.join(game.image_folder, 'player.png')).convert()
         self.image.set_colorkey((255, 255, 255))
         self.rect = self.image.get_rect()
-        self.rect.center = (game.width / 2, game.height / 2)
         self.game = game
+        self.health = 3
+        self.size = 30
         self.rect.x = x
         self.rect.y = y
 
-    #def draw(self):
-        #pygame.draw.rect(self.game.screen,
-                         #(210, 250, 251),
-                         #pygame.Rect(self.x, self.y, 8, 5))
+    def update(self):
+        for rocket in self.game.invader_rockets:
+            if (self.rect.x + self.size > rocket.x > self.rect.x - self.size and
+                    self.rect.y + self.size > rocket.y > self.rect.y - self.size):
+                self.game.invader_rockets.remove(rocket)
+                self.health -= 1
 
-
-class Generator:
-    def __init__(self, game):
-        vertical_margin = 30
-        horizontal_margin = 150
-        width = 50
-        row = -1
-        for y in range(horizontal_margin, int(game.height / 2), width):
-            row += 1
-            invaders_row = []
-            for x in range(vertical_margin, game.width - vertical_margin, width):
-                alien = Alien(game, x, y, row)
-                invaders_row.append(alien)
-                game.all_sprites.add(alien)
-            game.aliens.append(invaders_row)
-
-
-        # game.aliens.append(Alien(game, 280, 50))
 
 
 class Rocket:
@@ -141,6 +154,17 @@ class Rocket:
                          pygame.Rect(self.x, self.y, 2, 4))
         self.y -= 2
 
+class InvaderRocket:
+    def __init__(self, game, x, y):
+        self.x = x
+        self.y = y
+        self.game = game
+
+    def draw(self):
+        pygame.draw.rect(self.game.screen,
+                         (100, 255, 50),
+                         pygame.Rect(self.x, self.y, 2, 4))
+        self.y += 2
 
 if __name__ == '__main__':
     game = Game(600, 800)
